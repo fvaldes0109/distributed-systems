3862f6e519f6310123a7876aebd21ded75524975
fvaldes0109 <fvaldes0109@gmail.com>
2024-06-11 14:50:11 +0200

    Make functional load balancer with GET and POST requests

--- loadbalancer/index.js
+++ loadbalancer/index.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from load balancer!');
+// Read and parse the URLs from the environment variable
+const urls = process.env.BACKEND_SERVICES_URLS.split(',');
+
+app.get('*', (req, res) => {
+    const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+    res.redirect(randomUrl + req.url);
 });
 
-app.listen(8080, () => {
-    console.log('Load balancer is running on port 8080');
+app.post('*', (req, res) => {
+    const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+    res.redirect(307, randomUrl + req.url);
+});
+
+app.listen(process.env.LOAD_BALANCER_PORT, () => {
+    console.log('Load balancer is running on port ' + process.env.LOAD_BALANCER_PORT);
 });
